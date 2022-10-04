72c24f2be7f31ea597b8095dd035be0f20abd647
TuTuanLe <tutuanletkpt@gmail.com>
2022-10-04 20:43:54 +0700

    feature: init project bottender app

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = require('./src');
+
+
+module.exports = async function App(context) {
+    await context.sendText('Hello World tutuanle ');
+  };
+
---
+++ src/index.js
@@ -0,0 +1,3 @@
+module.exports = async function App(context) {
+  await context.sendText('Welcome to tutuanle');
+};
