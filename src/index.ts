5c3513f7f673622c934bbea84cf970b888c3d65d
TuTuanLe <tutuanletkpt@gmail.com>
2022-10-13 07:16:18 +0700

    config env

--- src/index.ts
+++ src/index.ts
@@ -6,41 +6,41 @@ export default async function App(contextT: MessengerContext): Promise<Action<Me
     async function HandleMessage(context: MessengerContext) {
         console.log(context.event.isText);
 
-        if (context.event.isText && !context.event.isEcho) {
-            if (context.event.isStandby) {
-                if (context.event.text === '/back') {
-                    await context.takeThreadControl();
-                    await context.sendText('Taking thread control back.');
-                }
-            } else if (context.event.text === '/help') {
-                await context.sendText('Passing thread control to the page inbox.');
-                await context.passThreadControlToPageInbox();
-            } else {
-                await context.sendText('Respond by bot.');
-            }
-        }
-
-        // if (context.event.isText && context.event.text === 'tutuanle') {
-        //     await context.sendButtonTemplate('Bạn muốn sử dụng dịch vụ nào của chúng tôi ?', [
-        //         {
-        //             type: 'postback',
-        //             title: 'Truy cập và mua tiêu chuẩn.',
-        //             payload: 'START_SERVICE_FIRST',
-        //         },
-        //         {
-        //             type: 'postback',
-        //             title: 'Các dịch vụ đăng ký trực tuyến',
-        //             payload: 'START_SERVICE_SECOND',
-        //         },
-        //         {
-        //             type: 'postback',
-        //             title: 'Các dich vụ về tiêu chuẩn',
-        //             payload: 'START_SERVICE_THREE',
-        //         },
-        //     ]);
-        // } else if (context.event.isText !== 'tutuanle') {
-        //     await context.sendText('mã của bạn không chính xác, vui lòng nhập lại mã !!');
+        // if (context.event.isText && !context.event.isEcho) {
+        //     if (context.event.isStandby) {
+        //         if (context.event.text === '/back') {
+        //             await context.takeThreadControl();
+        //             await context.sendText('Taking thread control back.');
+        //         }
+        //     } else if (context.event.text === '/help') {
+        //         await context.sendText('Passing thread control to the page inbox.');
+        //         await context.passThreadControlToPageInbox();
+        //     } else {
+        //         await context.sendText('Respond by bot.');
+        //     }
         // }
+
+        if (context.event.isText && context.event.text === 'tutuanle') {
+            await context.sendButtonTemplate('Bạn muốn sử dụng dịch vụ nào của chúng tôi ?', [
+                {
+                    type: 'postback',
+                    title: 'Truy cập và mua tiêu chuẩn.',
+                    payload: 'START_SERVICE_FIRST',
+                },
+                {
+                    type: 'postback',
+                    title: 'Các dịch vụ đăng ký trực tuyến',
+                    payload: 'START_SERVICE_SECOND',
+                },
+                {
+                    type: 'postback',
+                    title: 'Các dich vụ về tiêu chuẩn',
+                    payload: 'START_SERVICE_THREE',
+                },
+            ]);
+        } else if (context.event.isText !== 'tutuanle') {
+            await context.sendText('mã của bạn không chính xác, vui lòng nhập lại mã !!');
+        }
 
         if (context.event.isPayload) {
             await context.sendText(`received the payload: ${context.event.payload}`);
